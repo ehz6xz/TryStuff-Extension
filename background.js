811c9d3266b903c69c0008fc18fbf4d94e664a6c
eric Zhang <ericzhangxii@gmail.com>
2021-08-09 00:28:24 -0400

    First commit

---
+++ background.js
@@ -0,0 +1,12 @@
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+chrome.browserAction.onClicked.addListener(tab => {
+  chrome.tabs.create({
+    url: chrome.runtime.getURL("popup.html"),
+  });
+});
