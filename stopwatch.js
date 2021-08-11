fca7e41101849542f4a3869afb4d0ba62974fba9
zichael-ang <ikezhang66@gmail.com>
2021-08-11 13:42:20 -0400

    added nonworking timer, changed button functionality

--- stopwatch.js
+++ stopwatch.js
@@ -1,75 +1,46 @@
+var off = "#000000"
+var on = "#ffffff"
 var timerButton = document.getElementById("timerButton");
 var root = document.documentElement;
 var choiceText = document.getElementById("choiceText");
+var timerText = document.getElementById("timerText");
+var timerTimeContainer = document.getElementById("timerTimeContainer");
+var choiceContainer = document.querySelector(".choice-container")
 
-function A(e) {
-  choiceText.innerHTML = "RUNNING";
-}
-
-function B(e) {
-  choiceText.innerHTML = "NATURE";
-}
-
-function C(e) {
-  choiceText.innerHTML = "SLEEPING";
-}
-
-function D(e) {
-  choiceText.innerHTML = "MUSIC";
-}
-
-function E(e) {
-  choiceText.innerHTML = "VITAMIN D";
-}
-
-function F(e) {
-  choiceText.innerHTML = "COFFEE";
-}
-
-function G(e) {
-  choiceText.innerHTML = "MEDITATION";
-}
-
-function H(e) {
-  choiceText.innerHTML = "YOGA";
-}
-
-function I(e) {
-  choiceText.innerHTML = "NOTHING";
-}
-
+//Maps buttons to their functions
 var buttonMap = {
-  b_A: function(event) {
-    A(event.target)
+  b_A: function(e) {
+    choiceText.innerHTML = "RUNNING";
   },
-  b_B: function(event) {
-    B(event.target)
+  b_B: function(e) {
+    choiceText.innerHTML = "NATURE";
   },
-  b_C: function(event) {
-    C(event.target)
+  b_C: function(e) {
+    choiceText.innerHTML = "SLEEPING";
   },
-  b_D: function(event) {
-    D(event.target)
+  b_D: function(e) {
+    choiceText.innerHTML = "MUSIC";
   },
-  b_E: function(event) {
-    E(event.target)
+  b_E: function(e) {
+    choiceText.innerHTML = "SUPPLEMENTS";
   },
-  b_F: function(event) {
-    F(event.target)
+  b_F: function(e) {
+    choiceText.innerHTML = "COFFEE";
   },
-  b_G: function(event) {
-    G(event.target)
+  b_G: function(e) {
+    choiceText.innerHTML = "MEDITATION";
   },
-  b_H: function(event) {
-    H(event.target)
+  b_H: function(e) {
+    choiceText.innerHTML = "YOGA";
   },
-  b_I: function(event) {
-    I(event.target)
+  b_I: function(e) {
+    choiceText.innerHTML = "NOTHING";
   },
 }
 
-var container = document.querySelector(".choice-container")
-var buttonClick = container && container.addEventListener("click", (event) => {
+// event handler that all button-clicks pass their event to
+// then uses buttonMap to map clicks to their function
+var buttonClick = choiceContainer && choiceContainer.addEventListener("click", (event) => {
   var target = event.target;
   var handler;
   if (target.nodeName === "BUTTON" && (handler = target.getAttribute('data-handler'))) {
@@ -77,27 +48,38 @@ var buttonClick = container && container.addEventListener("click", (event) => {
   }
 })
 
-
-
-
-
-
+// Start/Stop button listener, changes color.
 timerButton.addEventListener("click", toggleTimer)
 
 function toggleTimer() {
-  var x = document.getElementById("timerText");
-  var off = "#000000"
-  var on = "#ffffff"
-  if (x.innerHTML === "START") {
-    x.innerHTML = "STOP";
-    root.style.setProperty('--on', off);
-    root.style.setProperty('--off', on);
+  if (timerText.innerHTML === "START") {
+    timerText.innerHTML = "STOP";
+    timerTimeContainer.style.display = '';
+    choiceContainer.style.display = 'none';
+    stopTimer();
   } else {
-    x.innerHTML = "START";
-    root.style.setProperty('--on', on);
-    root.style.setProperty('--off', off);
+    timerText.innerHTML = "START";
+    startTimer();
+    timerTimeContainer.style.display = 'none';
+    choiceContainer.style.display = 'grid';
   }
 }
+
+function startTimer() {
+  root.style.setProperty('--on', on);
+  root.style.setProperty('--off', off);
+}
+
+function stopTimer() {
+  root.style.setProperty('--on', off);
+  root.style.setProperty('--off', on);
+}
+
+function resumeTimer() {
+  root.style.setProperty('--on', off);
+  root.style.setProperty('--off', on);
+}
+
 
 function colorFadeChange(start, end) {
   
